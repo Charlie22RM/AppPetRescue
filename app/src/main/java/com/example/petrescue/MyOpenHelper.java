891b53618fcefe2ca4e3dee69072fcd09e978272
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2023-06-13 15:31:13 -0500

    Bd creada

--- app/src/main/java/com/example/petrescue/MyOpenHelper.java
+++ app/src/main/java/com/example/petrescue/MyOpenHelper.java
@@ -1,4 +1,29 @@
 package com.example.petrescue;
 
-public class MyOpenHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+
+    private static final String COMMENTS_TABLE_CREATE = "CREATE TABLE usuarios(_id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, apellido TEXT, email TEXT, password TEXT)";
+    private static final String DB_NAME = "datosPractica.sqlite";
+    private static final int DB_VERSION = 1;
+
+    public MyOpenHelper(Context context){
+
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+        db.execSQL(COMMENTS_TABLE_CREATE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
 }
+
