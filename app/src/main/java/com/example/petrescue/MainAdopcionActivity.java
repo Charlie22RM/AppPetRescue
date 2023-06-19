dcd0fbe7a76cfbebe35355d332992c55aaa4d2aa
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2023-06-19 16:51:49 -0500

    a

--- app/src/main/java/com/example/petrescue/MainAdopcionActivity.java
+++ app/src/main/java/com/example/petrescue/MainAdopcionActivity.java
@@ -6,14 +6,24 @@ import android.view.MenuInflater;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainAdopcionActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_adopcion);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menu_barra = getMenuInflater();
+        menu_barra.inflate(R.menu.menu, menu);
+        return true;
     }
 
 }
