513bceb18c0543e7d800a46560c6543e4798fd8f
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2023-06-19 21:53:49 -0500

    Cambios

--- app/src/main/java/com/example/petrescue/MainActivity.java
+++ app/src/main/java/com/example/petrescue/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.petrescue;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar_main = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menu_barra = getMenuInflater();
+        menu_barra.inflate(R.menu.menu, menu);
+        return true;
     }
 }
